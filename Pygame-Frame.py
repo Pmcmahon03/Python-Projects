a37d07ef17044e3fac207000bfd920b01ef1c3bf
Patrick McMahon <patrick.mcmahon.uk@gmail.com>
2021-11-18 16:02:45 +0000

    init

---
+++ Pygame-Frame.py
@@ -0,0 +1,28 @@
+import pygame, sys 
+
+#General Setup
+pygame.init()
+clock = pygame.time.Clock()
+
+#Main Window
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Game')
+
+while True:
+    #Handling input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Updating the window
+    pygame.display.flip()
+    clock.tick(60)
+
+
+
+
+
+#print("Hello World")
