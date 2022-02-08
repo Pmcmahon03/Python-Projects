9e623cb065ae2c02c0e7edc96c7c32d91c98bbf3
Patrick McMahon <patrick.mcmahon.uk@gmail.com>
2022-02-08 18:16:01 +0000

    Working version

--- FUTChampsCalculator.py
+++ FUTChampsCalculator.py
@@ -1,21 +1,49 @@
 targetlist=[]
+rewards = [4, 12, 24, 36, 45, 51, 60, 67, 72, 76]
+endscore = []
 
-startingpoints = 10
-gamesr = 6
+spts = int(input('Enter the number of points you have ?'))
+gamesr = int(input('how many games do you have remaining ?'))
 
-def genscore(gamesr):
+def genscore(gamesr,spts):
     [targetlist.append([gamesr-i,i,0,0]) for i in range(gamesr+1)]
+    fillscores(gamesr, spts)
 
-def scorecal(i):
+def fillscores(gamesr, spts):
+    [scorecal(i, spts) for i in range(gamesr+1)]
+
+def scorecal(i, spts):
     targetlist[i][2] = targetlist[i][0]*4+targetlist[i][1]*1
+    targetlist[i][3] = targetlist[i][2] + spts
 
-def fillscores(gamesr):
-    [scorecal(i) for i in range(gamesr+1)]
+def champscal(gamesr,spts,rewards):
 
-genscore(gamesr)
-fillscores(gamesr)
 
-print(targetlist)
+    genscore(gamesr,spts)
+    [endscore.append(targetlist[i][3]) for i in range(gamesr+1)]
+
+    match = list(set(rewards) & set(endscore))
+
+    if len(match) == 0:
+        print(f' You cannot reach target points')
+
+    else:
+        i = endscore.index(max(match))
+        print(f'Max score posibble = {max(match)} with {targetlist[i][0]} wins and {targetlist[i][1]} losses')
+
+champscal(gamesr,spts,rewards)
+
+
+
+
+
+    
+
+
+#genscore(gamesr)
+#fillscores(gamesr)
+
+#print(targetlist)
 
 
 
