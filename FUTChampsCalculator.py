30dc33fb5867aab5d3aaf15891841933fc78c779
Patrick McMahon <patrick.mcmahon.uk@gmail.com>
2022-02-08 13:39:03 +0000

    New approach to the calculator

--- FUTChampsCalculator.py
+++ FUTChampsCalculator.py
@@ -1,12 +1,27 @@
-import sys, random
+targetlist=[]
 
-class champspoints():
+startingpoints = 10
+gamesr = 6
 
-    def what_do_I_need(pointsNeeded, games):
-        
+def genscore(gamesr):
+    [targetlist.append([gamesr-i,i,0,0]) for i in range(gamesr+1)]
+
+def scorecal(i):
+    targetlist[i][2] = targetlist[i][0]*4+targetlist[i][1]*1
+
+def fillscores(gamesr):
+    [scorecal(i) for i in range(gamesr+1)]
+
+genscore(gamesr)
+fillscores(gamesr)
+
+print(targetlist)
 
 
-        pass
+
+
+
+
 
 
 
