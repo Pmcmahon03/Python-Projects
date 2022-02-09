2a0b6f006c70d2a32ee4c6e530481ae9873f2a49
Patrick McMahon <patrick.mcmahon.uk@gmail.com>
2022-02-09 16:10:40 +0000

    Version 1.0

--- FUTChampsCalculator.py
+++ FUTChampsCalculator.py
@@ -1,35 +1,32 @@
-targetlist=[]
+futmatrix = []
 rewards = [4, 12, 24, 36, 45, 51, 60, 67, 72, 76]
 endscore = []
 
 spts = int(input('Enter the number of points you have ?'))
 gamesr = int(input('how many games do you have remaining ?'))
 
-def genscore(gamesr,spts):
-    [targetlist.append([gamesr-i,i,0,0]) for i in range(gamesr+1)]
-    fillscores(gamesr, spts)
+def genmatrix(gamesr,spts):
+    [futmatrix.append([gamesr-i,i,0,0]) for i in range(gamesr+1)] # adds W L cols
+    [scorecal(i, spts) for i in range(gamesr+1)] #
 
-def fillscores(gamesr, spts):
-    [scorecal(i, spts) for i in range(gamesr+1)]
-
-def scorecal(i, spts):
-    targetlist[i][2] = targetlist[i][0]*4+targetlist[i][1]*1
-    targetlist[i][3] = targetlist[i][2] + spts
+def scorecal(i, spts): # adds w*4 + l*1 col  
+    futmatrix[i][2] = futmatrix[i][0]*4+futmatrix[i][1]*1
+    futmatrix[i][3] = futmatrix[i][2] + spts
 
 def champscal(gamesr,spts,rewards):
-
-
-    genscore(gamesr,spts)
-    [endscore.append(targetlist[i][3]) for i in range(gamesr+1)]
+   
+    genmatrix(gamesr,spts)
+    [endscore.append(futmatrix[i][3]) for i in range(gamesr+1)]
 
     match = list(set(rewards) & set(endscore))
 
     if len(match) == 0:
-        print(f' You cannot reach target points')
+        print(f'Your best score is {max(endscore)} your best reward is {max([i  for i in rewards if i < max(endscore)])}')
+        #
 
     else:
         i = endscore.index(max(match))
-        print(f'Max score posibble = {max(match)} with {targetlist[i][0]} wins and {targetlist[i][1]} losses')
+        print(f'Your max score = {max(endscore)} and your max rewards = {max(match)} with {futmatrix[i][0]} wins and {futmatrix[i][1]} losses')
 
 champscal(gamesr,spts,rewards)
 
